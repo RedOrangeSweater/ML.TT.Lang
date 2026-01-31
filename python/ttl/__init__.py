27255da58c16b616efbb0b576e04d05f37356985
Ilia Shutov <shutovilya@gmail.com>
2026-01-31 20:23:32 +0100

    chore(ttl): re-export is_runtime_tensor from ttl; doc note prefer is_runtime_tensor

--- python/ttl/__init__.py
+++ python/ttl/__init__.py
@@ -28,6 +28,7 @@ from ttl._generated_elementwise import __all__ as _elementwise_all
 
 # Export additional TTL DSL API classes
 from ttl.operators import signpost
+from ttl.runtime_tensor import is_runtime_tensor
 from ttl.ttl_api import (
     CircularBuffer,
     CopyTransferHandler,
@@ -38,6 +39,7 @@ __all__ = [
     "CircularBuffer",
     "CopyTransferHandler",
     "Program",
+    "is_runtime_tensor",
     "ProgramSpec",
     "TensorBlock",
     "compute",
